d0ba7f2a7980022336641387e4f904a3af3f9003
kuro <yanisEcolebellotEcole@gmail.com>
2026-02-18 09:46:23 +0100

    added user serializers and views

--- PrINTech-Back/back/apps/api/serializers.py
+++ PrINTech-Back/back/apps/api/serializers.py
@@ -1,1 +1,51 @@
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
+from models import User
+
+
+class ChangePasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(required=True, write_only=True)
+    new_password = serializers.CharField(required=True, write_only=True)
+    confirm_password = serializers.CharField(required=True, write_only=True)
+
+    def validate_old_password(self, password):
+        user = self.context["request"].user
+        if not user.check_password(password):
+            raise serializers.ValidationError("Wrong password")
+        return password
+
+    def validate(self, data):
+        if data["new_password"] != data["confirm_password"]:
+            raise serializers.ValidationError("Passwords do not match")
+        return data
+
+    def validate_new_password(self, password):
+        validate_password(password, user=self.context["request"].user)
+        return password
+
+    def save(self, **kwargs):
+        user = self.context["request"].user
+        user.set_password(self.validated_data["new_password"])
+        user.save()
+        return user
+
+class UserSerializer(serializers.ModelSerializer):
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            password=validated_data["password"],
+            email=validated_data["email"],
+        )
+        return user
+
+    # noinspection PyMethodMayBeStatic
+    def validate_password(self, password):
+        validate_password(password)
+        return password
+
+    class Meta:
+        model = User
+        fields = ["id", "username", "password", "email"]
+        extra_kwargs = {"password": {"write_only": True}}
+
